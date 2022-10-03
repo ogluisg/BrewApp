5b424ba0ea55b03d3c7f7c277e975e285ae59554
Luis Sanchez <Luisanchez9821@Gmail.com>
2022-10-02 20:01:52 -0500

    added TS hooks

--- app/redux/index.ts
+++ app/redux/index.ts
@@ -6,6 +6,10 @@ export const store = configureStore({
   reducer: {
     Beer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
